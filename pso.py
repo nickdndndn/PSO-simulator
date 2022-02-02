af0a53b266a0798a286c492f8ab1b45328e8fac7
nickd <nickd_96@hotmail.com>
2022-02-02 15:26:51 +0100

    Basic structure

---
+++ pso.py
@@ -0,0 +1,14 @@
+class PSO(N, c1, c2, Wmin, Wmax, Vmax, MaxIter):
+    def __init__(self):
+        #init population of N parameters
+        #maybe something like self.weights, self, pbest ...
+        return
+    def train(self):
+        ##for each partice calc, update priv and public best
+        ##update weights
+        #condition to stop
+        return self
+    def plot(self):
+        # save plots somehow
+    def predict(self):
+        #will we need this?
