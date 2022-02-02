738c6c4876cfbf58591b0d0ec1742f5a4223ca39
Elena Kane <59971317+elenakelly@users.noreply.github.com>
2022-02-02 17:59:52 +0100

    Update pso.py

--- pso.py
+++ pso.py
@@ -1,14 +1,40 @@
+class Particle():
+
+    def __init__(self):
+        # particle position, velocity
+        # best position & error (individual)
+        return
+    
+    def evaluate(self):
+        # check if current position is best
+        pass
+
+    def next(self):
+        #compute next position
+        #next velocity
+        pass
+
+
 class PSO(N, c1, c2, Wmin, Wmax, Vmax, MaxIter):
+
     def __init__(self):
         #init population of N parameters
         #maybe something like self.weights, self, pbest ...
+        # best position & error (group)
+        #optimize
         return
+
     def train(self):
         ##for each partice calc, update priv and public best
         ##update weights
         #condition to stop
         return self
+
     def plot(self):
         # save plots somehow
+        pass
+
     def predict(self):
         #will we need this?
+        pass
+
