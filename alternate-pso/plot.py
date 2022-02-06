ba9304cddc3440de949e9454e8b2d9110f1e858d
gmontymk33 <93831880+gmontymk33@users.noreply.github.com>
2022-02-06 16:33:55 +0100

    Add files via upload

--- alternate-pso/plot.py
+++ alternate-pso/plot.py
@@ -7,14 +7,12 @@ import os
 # Parameters are : swarm is the collection of the particles, function is the objective function that is optimised along with its parameters, iteration is the current iteration index
 
 
-def plotGraphs(swarm, function, function_parameters, iteration):
+def plotGraphs(swarm, function, iteration, plot_range):
 
     # For Rosenbrock works better with low values (e.g -2,2)
     # For Rastrigin, larger are better (e.g -100,100)
-    rangex = [-100, 100]
-    # For Rosenbrock works better with low values (e.g -1,3)
-    # For Rastrigin, larger are better (e.g -100,100)
-    rangey = [-100, 100]
+    rangex = [plot_range[0][0], plot_range[0][1]]
+    rangey = [plot_range[1][0], plot_range[1][1]]
 
     # Set up the graph for the function
     xlist = np.linspace(rangex[0], rangex[1], 100)
@@ -25,12 +23,12 @@ def plotGraphs(swarm, function, function_parameters, iteration):
 
     # Calculate the results of the function
     for i in range(len(X)):
-        Z.append(function([X[i], Y[i]], function_parameters))
+        Z.append(function[0]([X[i], Y[i]], function[1]))
 
     fig, ax = plt.subplots(1, 1)
     cp = ax.contour(X, Y, Z, 100)
     fig.colorbar(cp)
-    ax.set_title(function.__name__)
+    ax.set_title(function[0].__name__)
     ax.set_xlabel("Longitude")
     ax.set_ylabel("Langtitude")
     ax.set_xlim([rangex[0], rangex[1]])
@@ -43,6 +41,21 @@ def plotGraphs(swarm, function, function_parameters, iteration):
 
     # Save the graph as an image
     filename = f'images/Epoch_{iteration}_.png'
+    plt.savefig(filename, dpi=96)
+
+    # Cleanup
+    plt.close()
+
+    # Return the path to the file
+    return filename
+
+
+def plotLoss(global_error_plot):
+
+    plt.plot(global_error_plot[0], global_error_plot[1], 'ro')
+
+    # Save the graph as an image
+    filename = f'images/Loss.png'
     plt.savefig(filename, dpi=96)
 
     # Cleanup
--- alternate-pso/pso.py
+++ alternate-pso/pso.py
@@ -8,12 +8,14 @@ class Particle():
 
     def __init__(self, start_pos_vec, start_vlimit):
 
-        # Variables for  starting position/velocity, best position/velocity, error/best error
+        # Variables for  starting position/velocity, best position/velocity, error/best error, neighbourhood best position/error
         self.particle_pos = []
         self.particle_vel = []
         self.particle_bestPos = []
         self.particle_err = -1
         self.particle_bestErr = -1
+        self.neighbourhood_bestPos = []
+        self.neighbourhood_bestErr = -1
 
         # Initialise Starting position/velocity from parameters for each dimension
         for i in range(len(start_pos_vec)):
@@ -23,18 +25,37 @@ class Particle():
                 random.uniform(-start_vlimit, start_vlimit))
 
     # Evaluates the error of the function at the current position of the particle - essentially the distance from 0 since this is an optimisation problem
-    def evaluate(self, function, parameters):
+    def evaluate(self, function):
 
         # Returns the calculation of the function at the current position of the particle
-        self.particle_err = function(self.particle_pos, parameters)
+        self.particle_err = function[0](self.particle_pos, function[1])
 
         # Check if the error at the current position is less than the particle best. If yes, update the particle best position and error
         if self.particle_err < self.particle_bestErr or self.particle_bestErr == -1:
             self.particle_bestErr = self.particle_err
             self.particle_bestPos = list(self.particle_pos)
 
+    # Neighbourhood evaluation
+    def evaluate_neighbourhood(self, swarm, neighbourhood_range):
+
+        # Find which particles lie within the visibility range
+        for i in range(len(swarm)):
+            if swarm[i].particle_pos[0] < self.particle_pos[0]+neighbourhood_range and swarm[i].particle_pos[0] > self.particle_pos[0]-neighbourhood_range and swarm[i].particle_pos[1] < self.particle_pos[1]+neighbourhood_range and swarm[i].particle_pos[1] > self.particle_pos[1]-neighbourhood_range:
+                # For those that are, check whether their personal error is better than the neighbourhood error. If yes update current particle's neighbourhood error and position
+                if swarm[i].particle_bestErr < self.neighbourhood_bestErr or self.neighbourhood_bestErr == -1:
+                    self.neighbourhood_bestErr = swarm[i].particle_bestErr
+                    self.neighbourhood_bestPos = list(
+                        swarm[i].particle_bestPos)
+
     # Updates the velocity and position of the particle
-    def update(self, a, b, c, global_bestPos, vlimits):
+
+    def update(self, a, b, c, global_bestPos, vlimits, neighbourhood_toggle, slimits):
+
+        global_bestPos_update = []
+        if neighbourhood_toggle == True:
+            global_bestPos_update = list(self.neighbourhood_bestPos)
+        else:
+            global_bestPos_update = list(global_bestPos)
 
         # Update each dimension
         for i in range(len(self.particle_pos)):
@@ -46,7 +67,7 @@ class Particle():
             # PSO function
             self.particle_vel[i] = a * self.particle_vel[i] + b * r1 * \
                 (self.particle_bestPos[i] -
-                 self.particle_pos[i]) + c * r2 * (global_bestPos[i] - self.particle_pos[i])
+                 self.particle_pos[i]) + c * r2 * (global_bestPos_update[i] - self.particle_pos[i])
 
             # Clip velocity if it lies outside the thresholds
             if self.particle_vel[i] < -vlimits:
@@ -57,17 +78,25 @@ class Particle():
         # Update position of each dimension
         for i in range(len(self.particle_pos)):
             self.particle_pos[i] = self.particle_pos[i] + self.particle_vel[i]
+            # Clip position if it lies outside the thresholds
+            if self.particle_pos[i] > slimits:
+                self.particle_pos[i] = slimits
+            elif self.particle_pos[i] < -slimits:
+                self.particle_pos[i] = -slimits
 
-# Class that runs the main algorithm
+ # Class that runs the main algorithm
 
 
 class PSO():
 
     # Parameters are : Number of Particles, Dimensions of the function, The function itself and its parameters, The PSO parameters, Velocity limits and the Iteration number
-    def __init__(self, particle_number, dimensions, function, function_parameters, a, b, c, boundary,  start_vlimit, vlimits, max_iter):
+    def __init__(self, particle_number, dimensions, function, a, b, c, dynamic_parameter_adjustment, boundary, neighbourhood_options,  start_vlimit, vlimits, slimits, max_iter, plot_range):
 
         # Set Variable to keep the filenames of each plotted graph
-        filenames = []
+        filenames_graph = []
+
+        # Global error over epoch
+        global_error_plot = [[], []]
 
         # Variables for the global best position and the global best(lowest) error
         self.global_bestPos = []
@@ -79,7 +108,7 @@ class PSO():
             start_pos_vec = []
             # Randomise initial particle position of each of its dimensions within the parameter limit
             for j in range(dimensions):
-                start_pos_vec.append(random.uniform(-boundary, boundary))
+                start_pos_vec.append(random.uniform(boundary[0], boundary[1]))
             # Add the created particle to the eternal will of the swarm
             swarm.append(Particle(start_pos_vec, start_vlimit))
 
@@ -90,23 +119,78 @@ class PSO():
 
             # Evaluate each particle
             for k in range(len(swarm)):
-                swarm[k].evaluate(function, function_parameters)
+                swarm[k].evaluate(function)
 
                 # Update global best position / error if a better one is found
                 if swarm[k].particle_bestErr < self.global_bestErr or self.global_bestErr == -1:
                     self.global_bestErr = swarm[k].particle_bestErr
                     self.global_bestPos = list(swarm[k].particle_pos)
 
+            # Save global error for loss plot
+            global_error_plot[0].append(i)
+            global_error_plot[1].append(self.global_bestErr)
+
+            # Check whether neighbourhood options are activated
+
+            # Check if neighbourhood topology must switch to global topology
+            if (neighbourhood_options[5] == True):
+                if (i > neighbourhood_options[6]):
+                    neighbourhood_options[0] = False
+                    neighbourhood_options[2] = False
+
+            # Calculate neighbourhood best position / error per particle (If activated)
+            if neighbourhood_options[0] == True:
+                for l in range(len(swarm)):
+                    swarm[l].evaluate_neighbourhood(
+                        swarm, neighbourhood_options[1])
+
+            # Check if neighbourhood adjustment is activated and apply
+            if (neighbourhood_options[2] == True):
+                if (i % 10 == neighbourhood_options[3]):
+                    neighbourhood_options[1] = neighbourhood_options[1] + \
+                        neighbourhood_options[4]
+
+            # Adjust PSO Parameters
+            for idx_parameter in range(len(dynamic_parameter_adjustment)):
+                if idx_parameter == 0:
+                    # Adjust a
+                    a = adjust_parameters(
+                        a, dynamic_parameter_adjustment[idx_parameter], i)
+                elif idx_parameter == 1:
+                    # Adjust b
+                    b = adjust_parameters(b,
+                                          dynamic_parameter_adjustment[idx_parameter], i)
+                elif idx_parameter == 2:
+                    # Adjust c
+                    c = adjust_parameters(c,
+                                          dynamic_parameter_adjustment[idx_parameter], i)
+
             # Update each particle
             for m in range(len(swarm)):
-                swarm[m].update(a, b, c, self.global_bestPos, vlimits)
+                swarm[m].update(a, b, c, self.global_bestPos,
+                                vlimits, neighbourhood_options[0], slimits)
 
-            # Plot a graph and append the filename of the gunpoweder, treason and plot created
-            filenames.append(plot.plotGraphs(
-                swarm, function, function_parameters, i))
+            # Plot a graph and append the filename of the gunpowder, treason and plot created
+            filenames_graph.append(plot.plotGraphs(
+                swarm, function, i, plot_range))
 
             # Continue
             i += 1
 
+        # Plot the loss graph in relation to the global error
+        plot.plotLoss(global_error_plot)
+
         # Create a Gif from all the graphs plotted
-        plot.createGif(filenames)
+        plot.createGif(filenames_graph)
+
+
+def adjust_parameters(parameter, options, iteration):
+    if (options[0] > 0):
+        if (iteration % 10 == options[1]):
+            if (options[0] == 1 and parameter < options[3]):
+                parameter = parameter + \
+                    options[2]
+            elif (options[0] == 2 and parameter > options[3]):
+                parameter = parameter - \
+                    options[2]
+    return parameter
